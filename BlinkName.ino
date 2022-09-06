d1e0440141bf6bf42ca954cd30ae382267bfb1e6
kanishkjain0263 <103958932+kanishkjain0263@users.noreply.github.com>
2022-09-06 18:45:48 +0530

    Add files via upload

---
+++ BlinkName.ino
@@ -0,0 +1,56 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void dot() {   //Method to blink a short burst of light to represent a dot
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(200);
+}
+
+void dash() {   //Method to blink a long burst of light to represent a dash
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(600);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(600);
+}
+
+void loop() {
+  //Writing the word "K A N I S H K" in morse code
+
+//  K
+  dash();
+  dot();
+  dash();
+  delay(1000);
+//  A
+    dot();
+    dash();
+  delay(1000);    
+//  N
+  dash();
+  dot();
+  delay(1000);
+//  I
+  dot();
+  dot();
+    delay(1000);
+//  S
+  dot();
+  dot();
+  dot();
+    delay(1000);
+//  H
+  dot();
+  dot();
+  dot();
+  dot();
+    delay(1000);
+//  K
+  dash();
+  dot();
+  dash();
+    delay(1000);
+}
